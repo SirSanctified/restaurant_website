c90e7472ae08d4440a9612a59a16dccea083585c
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-06-06 22:25:07 +0200

    Implement the footer

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -11,8 +11,8 @@ const FindUs = () => (
       <div className="app__wrapper-content">
         <p className="p__opensans">Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath CW9 75G</p>
         <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0'}}>Opening Hours</p>
-        <p className="p__opensans">Mon - Fri: 10:00AM - 02:00AM</p>
-        <p className="p__opensans">Sat - Sun: 10:00AM - 03:00AM</p>
+        <p className="p__opensans">Mon - Fri: 08:00AM - 12:00AM</p>
+        <p className="p__opensans">Sat - Sun: 07:00AM - 11:00PM</p>
       </div>
       <button type='button' className='custom__button' style={{ marginTop: '2rem' }}>Visit Us</button>
     </div>
